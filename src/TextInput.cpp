5500c506fea32e158e13d110c040691858c7ff21
Advik <advik.b@gmail.com>
2024-05-02 20:46:36 +0530

    Update TextInput.cpp

--- src/TextInput.cpp
+++ src/TextInput.cpp
@@ -8,6 +8,24 @@
 typedef std::vector<std::string> Text;
 
 class TextBox {
+    public:
+        TextBox();
+        ~TextBox();
+        void update(sf::Event &event);
+        void draw_to(sf::RenderWindow &window);
+        void setPosition(sf::Vector2f position);
+        void setScale(sf::Vector2f scale);
+        void setFont(sf::Font font);
+        void setTextColor(sf::Color textColor);
+        void setOutlineColor(sf::Color outlineColor);
+        void setBackgroundColor(sf::Color backgroundColor);
+        void setText(Text text);
+        sf::Vector2f getPosition();
+        sf::Vector2f getScale();
+        sf::Font getFont();
+        sf::Color getTextColor();
+        sf::Color getOutlineColor();
+        sf::Color getBackgroundColor();
     private:
         sf::Vector2f position;
         sf::Vector2f scale;
