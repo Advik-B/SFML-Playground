880d215ea1160e6e67e991fd6ccef11efcad90db
Advik <advik.b@gmail.com>
2024-05-05 13:19:35 +0530

    Update Button.cpp

--- src/Button.cpp
+++ src/Button.cpp
@@ -41,7 +41,7 @@ namespace UI {
         float x = pos.x + this->shape.getGlobalBounds().width / 2.f - this->text.getGlobalBounds().width / 2.f;
         float y = pos.y + this->shape.getGlobalBounds().height / 2.f - this->text.getGlobalBounds().height / 2.f;
 
-        this->text.setPosition({x, y});
+        this->text.setPosition({x - 20, y});
     }
 
     void Button::setString(const std::string& str) {
@@ -69,6 +69,49 @@ namespace UI {
             }
         }
         return false;
+    }
+
+    void Button::setSize(const sf::Vector2f &size) {
+        this->shape.setSize(size);
+        // Calculate the new position of the text
+        float x = this->shape.getPosition().x + this->shape.getGlobalBounds().width / 2.f - this->text.getGlobalBounds().width / 2.f;
+        float y = this->shape.getPosition().y + this->shape.getGlobalBounds().height / 2.f - this->text.getGlobalBounds().height / 2.f;
+        this->text.setPosition({x, y});
+
+        // Calculate the new font size of the text
+        unsigned int newSize = this->text.getCharacterSize();
+        while (getFontSizes(this->font, this->text.getString(), newSize) > this->shape.getGlobalBounds().width) {
+            newSize--;
+        }
+        this->text.setCharacterSize(newSize);
+    }
+
+    float Button::getFontSizes(const sf::Font &font, const std::string &str, unsigned int size) {
+        sf::Text text;
+        text.setFont(font);
+        text.setCharacterSize(size);
+        text.setString(str);
+        return text.getGlobalBounds().width;
+    }
+
+    void Button::setOutlineColor(const sf::Color &color) {
+        this->shape.setOutlineColor(color);
+    }
+
+    void Button::setFontStyle(sf::Text::Style style) {
+        this->text.setStyle(style);
+    }
+
+    void Button::setOutlineThickness(float thickness) {
+        this->shape.setOutlineThickness(thickness);
+    }
+
+    void Button::setFontSize(unsigned int size) {
+        // Calculate the new position of the text and set the new font size
+        this->text.setCharacterSize(size);
+        float x = this->shape.getPosition().x + this->shape.getGlobalBounds().width / 2.f - this->text.getGlobalBounds().width / 2.f;
+        float y = this->shape.getPosition().y + this->shape.getGlobalBounds().height / 2.f - this->text.getGlobalBounds().height / 2.f;
+        this->text.setPosition({x, y});
     }
 
 }
