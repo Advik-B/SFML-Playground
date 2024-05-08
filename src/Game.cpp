3e79a8fac04d039bfdb2a7c1e7cb9be9c836796b
Advik <advik.b@gmail.com>
2024-05-08 14:48:07 +0530

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include "Game.hpp"
 #include "fonts/Roboto-Regular.hpp"
-#include "pratibha.hpp"
+
 #include "icecream.hpp"
+
+#include "UI.cpp"
 
 void Game::initVariables() {
     this->window = nullptr;
+    this->currentGrab = new sf::Texture();
     sf::Font font;
     if (!font.loadFromMemory(Roboto, sizeof Roboto /  sizeof Roboto[0])) {
         std::cerr << "Error loading font" << std::endl;
         exit(-1);
     }
-//    auto* texture = new sf::Texture;
-//    if (!texture->loadFromMemory(pratibha, sizeof pratibha / sizeof pratibha[0])) {
-//        std::cerr << "Error loading texture" << std::endl;
-//        exit(-1);
-//    }
     this->button.setFont(font);
     this->button.setPosition({100.f, 100.f});
-    this->button.setString("Hello World");
-    this->button.setTextColor(sf::Color::Black);
-//    this->button.setTexture(texture);
+    this->button.setString("This is a button!");
     this->button.setSize({200.f, 100.f});
     this->button.setOutlineThickness(2.f);
-    this->button.setFontSize(30);
-//    this->button.setOutlineColor(sf::Color::White);
-
-
+    this->button.setFontSize(20.f);
 }
 
 void Game::initWindow() {
@@ -34,10 +27,14 @@ void Game::initWindow() {
     this->window = new sf::RenderWindow(this->videoMode, "SFML with C++");
     this->window->setVerticalSyncEnabled(true);
 }
+void Game::takeScreenshot() const {
+    this->currentGrab->create(this->window->getSize().x, this->window->getSize().y);
+    this->currentGrab->update(*this->window);
+}
 
 Game::Game() {
     initVariables();
-    initWindow();
+    initWindow(); // why not initVariables() first?
 }
 
 Game::~Game() {
@@ -53,6 +50,7 @@ void Game::render() {
     this->window->clear({37, 150, 190});
 
     this->button.render_to(*this->window);
+    this->window->draw(this->circle);
 
     this->window->display();
 }
@@ -71,10 +69,15 @@ while (this->window->pollEvent(this->ev)) {
             case sf::Event::Resized:
                 this->window->setView(sf::View(sf::FloatRect(0.f, 0.f, ev.size.width, ev.size.height)));
                 break;
-            default:
+            case sf::Event::MouseMoved:
+                this->button.update(sf::Mouse::getPosition(*this->window), *this->window);
+                break;
+            case sf::Event::MouseButtonPressed:
                 if (this->button.isPressed(ev)) {
                     IC();
                 }
+                break;
+            default:
                 break;
         }
     }
@@ -83,5 +86,7 @@ while (this->window->pollEvent(this->ev)) {
 void Game::update() {
     this->clock.restart();
     this->pollEvents();
-    this->button.update(sf::Mouse::getPosition(*this->window), *this->window);
+    this->takeScreenshot();
+    this->button.setTexture(currentGrab);
+
 }
